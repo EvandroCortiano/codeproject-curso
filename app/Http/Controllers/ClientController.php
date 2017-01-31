72a5238c8c7f0e1c63b059ef41ddc072c1585efa
Evandro C Cortiano <evandrocortiano@hotmail.com>
2017-01-31 20:21:18 -0200

    Subindo projeto CodeProject etapa 1 do Curso

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace CodeProject\Http\Controllers;
+
+use CodeProject\Client;
+
+use Illuminate\Http\Request;
+
+
+class ClientController extends Controller
+{
+    public function index(){
+    	return \CodeProject\Client::all();
+	}
+    
+	public function store(Request $request){
+		return Client::create($request->all());
+	}
+	
+	public function show($id){
+		return Client::find($id);
+	}
+	
+	public function destroy($id){
+		Client::find($id)->delete();
+	}
+	
+	public function store(Request $request){
+		return Client::create($request->all());
+	}
+}
