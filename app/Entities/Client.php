5dfd8a6b33c3f24d01ce7255db30daf381b7ac11
Evandro PcDial <evandrocortiano@gmail.com>
2017-03-21 08:13:38 -0300

    Ajuste finalizando Back-End

--- app/Entities/Client.php
+++ app/Entities/Client.php
@@ -4,6 +4,7 @@ namespace CodeProject\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
 
 class Client extends Model implements Transformable{
 	
